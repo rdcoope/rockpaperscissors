2cf7b934a1af3a69a9084acb1f1fb616a1997075
Richard <rdcoope@gmail.com>
2023-12-26 14:53:55 -0600

    Add testing prompt

---
+++ testing.js
@@ -0,0 +1,6 @@
+let promptAnimal = prompt("What is your favorite animal?")
+function animalTesting(animal){
+   return animal + " is my favorite animal"
+}
+
+console.log(animalTesting(promptAnimal));
