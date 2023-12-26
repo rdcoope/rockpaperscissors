17b51aa8fd0d808a010b9415fbbea7e0622eb17d
Richard <rdcoope@gmail.com>
2023-12-26 14:16:14 -0600

    Play a single round of RPS (infinitely)

--- javascript.js
+++ javascript.js
@@ -2,10 +2,8 @@ function getComputerChoice(){
     let number = Math.floor(Math.random() * 3) + 1 ;
     let choice;
     
-    if (number === 1){
-        choice = "Rock";
-    } else if (number === 2){
-        choice = "Paper"; 
+    if (number === 1){ choice = "Rock";
+    } else if (number === 2){ choice = "Paper"; 
     } else {choice = "Scissors"; }
 
     // console.log(number);
@@ -29,5 +27,46 @@ function getPlayerChoice(){
     return choice;
 }
 
-console.log("Computer :: " + getComputerChoice());
-console.log("Player :: " + getPlayerChoice());
+function playRound() {
+    let winner;
+    let player = getPlayerChoice();
+    let computer = getComputerChoice();
+
+    if (player === "Rock" && computer === "Rock"){
+        winner = "Tie, Please play again!";
+    } else if (player === "Rock" && computer === "Paper"){
+        winner = "You Lose! Paper beats Rock";
+    } else if (player === "Rock" && computer === "Scissors") {
+        winner = "You Win! Rock beats Scissors";
+    } else if (player === "Paper" && computer === "Rock"){
+        winner = "You Win! Paper beats Rock";
+    } else if (player === "Paper" && computer === "Paper"){
+        winner = "Tie, Please play again!";
+    } else if (player === "Paper" && computer === "Scissors") {
+        winner = "You Lose! Scissors beats Paper";
+    } else if (player === "Scissors" && computer === "Rock"){
+        winner = "You Lose! Rock beats Paper";
+    } else if (player === "Scissors" && computer === "Paper"){
+        winner = "You Win! Scissors beats Paper";
+    } else if (player === "Scissors" && computer === "Scissors") {
+        winner = "Tie, Please play another Round";
+    } else {
+        winner = "Choice unknown, Please choose a valid option."
+    }
+
+
+
+
+    console.log(computer);
+    console.log(player);
+    console.log(winner);
+
+    return winner;
+}
+
+// console.log("Computer :: " + getComputerChoice());
+// console.log("Player :: " + getPlayerChoice());
+
+while(true){
+    alert("The winner of this round is: " + playRound())
+}
