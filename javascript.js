a0cab179024933ea0c7f5a577e63fd27cf94118a
Richard <rdcoope@gmail.com>
2023-12-26 13:25:53 -0600

    Give players a choice in weapon

--- javascript.js
+++ javascript.js
@@ -1,13 +1,33 @@
 function getComputerChoice(){
     let number = Math.floor(Math.random() * 3) + 1 ;
     let choice;
+    
     if (number === 1){
         choice = "Rock";
     } else if (number === 2){
         choice = "Paper"; 
     } else {choice = "Scissors"; }
 
+    // console.log(number);
+    // console.log(choice);
+    return choice;
+}
+function getPlayerChoice(){
+    let playerChoice = prompt("Choose Rock (1), Paper (2), or Scissors (3)");
+    let choice;
+
+    if (playerChoice.toLowerCase() === "rock" || playerChoice.toLowerCase() === "1" ) {
+        choice = "Rock";
+    } else if (playerChoice.toLowerCase() == "paper" || playerChoice === "2") {
+        choice = "Paper";
+    } else if (playerChoice.toLowerCase() === "scissors" || playerChoice === "3") {
+        choice = "Scissors";
+    }
+
+    // console.log(playerChoice);
+    // console.log(choice);
     return choice;
 }
 
-console.log("Computer Chooses " + getComputerChoice())
+console.log("Computer :: " + getComputerChoice());
+console.log("Player :: " + getPlayerChoice());
