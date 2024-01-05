df604aae1e777496f1e24776ba681ada27f7f020
Richard <rdcoope@gmail.com>
2024-01-05 09:33:57 -0600

    Get stuck with event Listeners Im currently stuck trying to pull the click event. going to do a rewrite of the code in general to make things easier to understand then i will do the event handlers wish me luck

--- javascript.js
+++ javascript.js
@@ -13,74 +13,88 @@ function getComputerChoice(){
     // console.log(choice);
     return choice;
 }
+function gameRound(player){
+    //let player
+    
+        //playerName = "player";
+        player = playerChoice;
+        computer = getComputerChoice();
+        //computerName = "Computer"
+        
+        if (player === "Rock" && computer === "Rock"){
+            winnerPlayer = 3;
+        } else if (player === "Rock" && computer === "Paper"){
+            winnerPlayer = 2;
+        } else if (player === "Rock" && computer === "Scissors") {
+            winnerPlayer = 1;
+        } else if (player === "Paper" && computer === "Rock"){
+            winnerPlayer = 1;
+        } else if (player === "Paper" && computer === "Paper"){
+            winnerPlayer = 3;
+        } else if (player === "Paper" && computer === "Scissors") {
+            winnerPlayer = 2;
+        } else if (player === "Scissors" && computer === "Rock"){
+            winnerPlayer = 2;
+        } else if (player === "Scissors" && computer === "Paper"){
+            winnerPlayer = 1;
+        } else if (player === "Scissors" && computer === "Scissors") {
+            winnerPlayer = 3;
+        } else {
+            winnerPlayer = 4
+        }
+    
+        return winnerPlayer;
+    
+    }
 
+    const rock = document.querySelector('#rock')
+   rock.addEventListener('click',function() {
+        const value = this.value;
+   })
+    console.log(value)
+
+/*
 const btn = document.querySelector('#playerChoose');
-//console.log(btn)
-let playerChoice = btn.addEventListener('click', (event) => {
+btn.addEventListener('click', (event) => {
     let target = event.target;
     
+    target.id
     //console.log(target.id);
 
-    switch(target.id) {
-        case 'rock':
-            playerChoice = 'Rock'
-            console.log(playerChoice);
+     switch (target.id) {
+        case 'rock': 
+           playerChoice = 'Rock'
+            //console.log(test)
             break;
         case 'Paper':
-            playerChoice = 'Paper'
-            console.log(playerChoice);
+            playerChoice = 'Paper';
             break;
-        case 'Scissors':
-            playerChoice = 'Scissors'
-            console.log(playerChoice);
+        case 'Scissors': 
+            playerChoice = 'Scissors';
             break;
-            
-
+        
     }
-    return playerChoice
-});
-console.log(playerChoice)
-
-playerChoice
-function gameRound(playerName, playerChoice, computer){
-
-
-    playerName = "player";
-    player = playerChoice;
-    computer = getComputerChoice();
-    computerName = "Computer"
+    //console.log(test)
     
-    if (player === "Rock" && computer === "Rock"){
-        winnerPlayer = 3;
-    } else if (player === "Rock" && computer === "Paper"){
-        winnerPlayer = 2;
-    } else if (player === "Rock" && computer === "Scissors") {
-        winnerPlayer = 1;
-    } else if (player === "Paper" && computer === "Rock"){
-        winnerPlayer = 1;
-    } else if (player === "Paper" && computer === "Paper"){
-        winnerPlayer = 3;
-    } else if (player === "Paper" && computer === "Scissors") {
-        winnerPlayer = 2;
-    } else if (player === "Scissors" && computer === "Rock"){
-        winnerPlayer = 2;
-    } else if (player === "Scissors" && computer === "Paper"){
-        winnerPlayer = 1;
-    } else if (player === "Scissors" && computer === "Scissors") {
-        winnerPlayer = 3;
-    } else {
-        winnerPlayer = 4
-    }
+   console.log(playerChoice)
 
-    return winnerPlayer;
 
-}
+//console.log(playerChoice)
+
+//player = playerChoice
+gameRound(playerChoice)
+
+});
+*/
 
 // console.log("Computer :: " + getComputerChoice());
 // console.log("Player :: " + getPlayerChoice());
 
-let roundWin = gameRound();
+let roundWin = gameRound( playerChoice );
 console.log(roundWin);
+
+
+
 /*
 function game(roundWin , playerName){
     let playerWins, computerWins, tieWins;
