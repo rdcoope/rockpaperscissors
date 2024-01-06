3006f51aed7dae1561874377fac78984079ba0df
Richard <rdcoope@gmail.com>
2024-01-06 11:19:17 -0600

    Get Scores working properly

--- javascript.js
+++ javascript.js
@@ -24,6 +24,7 @@ console.log('Is the game over? ' + isGameOver());
 
 let computerChoice = getComputerChoice();
 function playRound(playerChoice,computerChoice){
+    let roundWinner = ""
     if (playerChoice === computerChoice){roundWinner = 'Tie'
     tieCount ++ ;
 }
@@ -31,67 +32,47 @@ function playRound(playerChoice,computerChoice){
         (playerChoice === 'Rock' && computerChoice === 'Scissors') ||
         (playerChoice === 'Paper' && computerChoice === 'Rock') ||
         (playerChoice === 'Scissors' && computerChoice === 'Paper')){
-            playerScore ++ };
+            playerScore ++
+            roundWinner = "Player";
+        
+        };
     if (
         (playerChoice === 'Paper' && computerChoice === 'Scissors') ||
         (playerChoice === 'Scissors' && computerChoice === 'Rock') ||
         (playerChoice === 'Rock' && computerChoice === 'Paper')){
-            computerScore ++ };
+            computerScore ++ ;
+            roundWinner = 'Computer';
+        };
     roundCount ++;
-}
-const btn = document.querySelector('#playerChoose');
-btn.addEventListener('click',function getPlayerChoice(event)  {
-    let target = event.target
-    let playerChoice
-    switch (target.id){
-        case ('Rock'): playerChoice = 'Rock';
-        case ('Paper'): playerChoice = 'Paper';
-        case ('Scissors'): playerChoice ='Scissors';
-    }
-    
-})
-
-    // ******** Winner! / gameplay
-function winnerFunction(){
-    let winner = ''
-    while (playerScore < 5 && computerScore < 5 && roundCount < 20) {
-    //playRound(getPlayerChoice(),getComputerChoice())
-playRound('Rock', getComputerChoice())
-
-    console.log('**************')
-
-    console.log('Player: ' + playerScore)
-    console.log('Computer: ' + computerScore)
-    console.log('Tie: ' + tieCount)
-
-    console.log('Round Count: ' + roundCount)
-
-    console.log('**************')
-
-    }
-    if (playerScore > computerScore){
-        winner = 'Player'
-    }else{
-        winner = 'Computer'
-    }
-    return winner
+    return roundWinner
 }
 
-//    console.log('Victory goes to ' + winnerFunction())
- //   console.log('Player: ' + playerScore)
-//    console.log('Computer: ' + computerScore)
-//    console.log('Tie: ' + tieCount)
 
 
 
-winnerFunction()
 
-
-// ******** UI
+// ******** UI ********* //
 const  player = document.querySelector('#playerScore');
 const computerUi = document.querySelector('#computerScore')
 const tie = document.querySelector('#tie');
 const roundUI = document.querySelector('#roundCount')
+const winnerUi = document.querySelector('#roundPlay')
+
+
+if (playerScore > computerScore){
+    winner = 'Player'
+}else if (computerScore > playerScore ){
+    winner = 'Computer'
+}else{
+    winner = ""
+    winnerUi.textContent = "It's a Tie, Keep Playing"
+}
+console.log(winner)
+
+
+winnerUi.textContent = 'The winner is: ' + winner 
+
+
 
 const divComputer = document.createElement("div")
 const divPlayer = document.createElement('div')
@@ -119,137 +100,42 @@ divRound.textContent = roundCount
 roundUI.appendChild(divRound)
 console.log(roundUI)
 
-
-/*
-// ********* Works in progress *********
-function getPlayerChoice(choice){
-    switch(choice){
-        case 0: return 'Rock';
-        case 1: return 'Paper';
-        case 2: return 'Scissors';
-    }
-};
-
-console.log(getPlayerChoice())
-*/
-/*
-function gameRound(player){
-    //let player
-    
-        //playerName = "player";
-        player = playerChoice;
-        computer = getComputerChoice();
-        //computerName = "Computer"
-        
-        if (player === "Rock" && computer === "Rock"){
-            winnerPlayer = 3;
-        } else if (player === "Rock" && computer === "Paper"){
-            winnerPlayer = 2;
-        } else if (player === "Rock" && computer === "Scissors") {
-            winnerPlayer = 1;
-        } else if (player === "Paper" && computer === "Rock"){
-            winnerPlayer = 1;
-        } else if (player === "Paper" && computer === "Paper"){
-            winnerPlayer = 3;
-        } else if (player === "Paper" && computer === "Scissors") {
-            winnerPlayer = 2;
-        } else if (player === "Scissors" && computer === "Rock"){
-            winnerPlayer = 2;
-        } else if (player === "Scissors" && computer === "Paper"){
-            winnerPlayer = 1;
-        } else if (player === "Scissors" && computer === "Scissors") {
-            winnerPlayer = 3;
-        } else {
-            winnerPlayer = 4
-        }
-    
-        return winnerPlayer;
-    
-    }
-
-    const rock = document.querySelector('#rock')
-   rock.addEventListener('click',function() {
-        const value = this.value;
-   })
-    console.log(value)
-
-
-const btn = document.querySelector('#playerChoose');
-btn.addEventListener('click', (event) => {
-    let target = event.target;
-    
-    target.id
-    //console.log(target.id);
-
-     switch (target.id) {
-        case 'rock': 
-           playerChoice = 'Rock'
-            //console.log(test)
-            break;
-        case 'Paper':
-            playerChoice = 'Paper';
-            break;
-        case 'Scissors': 
-            playerChoice = 'Scissors';
-            break;
-        
-    }
-    //console.log(test)
-    
-   console.log(playerChoice)
-
-
-//console.log(playerChoice)
-
-//player = playerChoice
-gameRound(playerChoice)
-
-});
-
-
-// console.log("Computer :: " + getComputerChoice());
-// console.log("Player :: " + getPlayerChoice());
-
-let roundWin = gameRound( playerChoice );
-console.log(roundWin);
-
-
-
-/*
-function game(roundWin , playerName){
-    let playerWins, computerWins, tieWins;
-    playerWins = 0;
-    computerWins = 0;
-    tieWins = 0;
-    roundWin = gameRound();
-    //branch testing
-
-// how is this loop functional. When using || (or) the game continues on 
-// to infinity, but when switching to && (and) the game stops at 5 wins
-    while (computerWins < 5 && playerWins < 5 && tieWins < 5 ) {
-        if (gameRound() === 1){
-            playerWins++;
-        } else if (gameRound() === 2){
-            computerWins++;
-        } else if (gameRound() === 3){
-            tieWins++;
-        } else {
-            console.log("Try again, that is not a valid response.");
-        }
-
-        console.log("Player: " + playerWins);
-        console.log("Computer: " + computerWins);
-        console.log("Ties: " + tieWins);
-    }
-
-
-   
-
-
-
-    return playerWins, computerWins, tieWins;
-
+function scoring(round) {
+    /*
+    if ( round = 'Player') {
+        //playerScore ++
+       }else if (round = 'Computer') {
+       // computerScore ++
+       }else {tieCount++}
+    */
+divPlayer.textContent = playerScore
+divComputer.textContent = computerScore 
+divTie.textContent = tieCount
+//roundCount++
+divRound.textContent = roundCount
+//console.log(playerScore + computerScore + tieCount + roundCount)
 }
 
-game();
+
+
+const btn = document.querySelector('#playerChoose')
+btn.addEventListener('click', (event) => {
+    let target = event.target
+    switch (target.id) {
+        case ('Rock'): scoring(playRound('Rock',getComputerChoice()));
+        break;
+        case ('Paper'): scoring(playRound('Paper',getComputerChoice()));
+        break;
+        case ('Scissors'): scoring(playRound('Scissors',getComputerChoice()));
+        break
+    }
+})
+
+if ()
+/*
+const rockBtn = document.querySelector('#Rock')
+rockBtn.addEventListener('click',() => {
+    scoring(playRound('Rock', getComputerChoice()))
+})
+console.log(playerScore)
 */
